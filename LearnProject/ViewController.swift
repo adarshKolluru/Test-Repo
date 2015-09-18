e3d2dd1607b7ff3f792580b574d1cd8df019d821
adarshKolluru <@aarshchowdhary@gmail.com>
2015-09-18 12:19:22 +0530

    Initial Commit

---
+++ LearnProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnProject
+//
+//  Created by Praveen Yadav on 9/18/15.
+//  Copyright (c) 2015 zemoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
